a95e308ea2e742dcc0225d6bee4c8eacb43ed9ed
Tanner Taylor <tanner@ttpro.dev>
2022-12-02 22:47:35 -0700

    Completed Day 1, 2022

---
+++ 2022/day-01/Program.cs
@@ -0,0 +1,25 @@
+﻿var numElves = args.Any() && int.TryParse(args[0], out var value) ? value : 1;
+
+var entries = File.ReadAllLines("input1.txt");
+var chunkedEntries = ChunkEntries(entries);
+var sums = chunkedEntries.Select(chunk => chunk.Sum(calories => Convert.ToInt32(calories)));
+var max = sums.OrderDescending().Take(numElves).Sum();
+Console.WriteLine(max);
+
+IEnumerable<IEnumerable<string>> ChunkEntries(IEnumerable<string> entries)
+{
+    var enumerator = entries.GetEnumerator();
+    while (enumerator.MoveNext())
+        yield return CreateChunk(enumerator);
+}
+
+IEnumerable<string> CreateChunk(IEnumerator<string> enumerator)
+{
+    do
+    {
+        if (string.IsNullOrEmpty(enumerator.Current))
+            yield break;
+        yield return enumerator.Current;
+    }
+    while (enumerator.MoveNext());
+}
