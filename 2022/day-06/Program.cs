457beaca1ede10af4005ed9f53e1f5fc607a7c73
Tanner Taylor <tanner@ttpro.dev>
2022-12-06 09:04:07 -0700

    Completed Day 6, 2022.

---
+++ 2022/day-06/Program.cs
@@ -0,0 +1,26 @@
+﻿var numDistinctChars = args.Any() && args[0] == "part2" ? 14 : 4;
+
+var chars = File.ReadAllLines("input.txt").Single().ToCharArray();
+
+var last4Chars = new Queue<char>(chars.Take(numDistinctChars));
+if (TryPrintMarker(numDistinctChars - 1))
+    return;
+
+for (var i = numDistinctChars; i < chars.Length; i++)
+{
+    last4Chars.Dequeue();
+    last4Chars.Enqueue(chars[i]);
+    if (TryPrintMarker(i))
+        return;
+}
+
+bool TryPrintMarker(int index)
+{
+    if (last4Chars!.Distinct().Count() == numDistinctChars)
+    {
+        Console.WriteLine(index + 1);
+        return true;
+    }
+
+    return false;
+}
